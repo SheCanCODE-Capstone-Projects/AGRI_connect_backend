3d9bb42adda94857e6f25367a618569185ebc078
IKelia <mbonimpakelia@gmail.com>
2026-07-22 02:33:21 -0700

    docs: Add Schema documentation to LoginRequest DTO

--- src/main/java/com/scc/Agriconnect/dto/LoginRequest.java
+++ src/main/java/com/scc/Agriconnect/dto/LoginRequest.java
@@ -1,15 +1,19 @@
 package com.scc.Agriconnect.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
+@Schema(description = "Request payload for user authentication")
 public class LoginRequest {
 
+    @Schema(description = "Email address of the user", example = "president@example.com", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank @Email
     private String email;
 
+    @Schema(description = "User password", example = "SecurePass123", requiredMode = Schema.RequiredMode.REQUIRED)
     @NotBlank
     private String password;
 }
